7fc9dda8f72d1cb037c7b752d616f00a3df5e4bf
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-04-05 14:29:35 +0600

    new commite

--- src/components/TECHFORING/TECHFORING.jsx
+++ src/components/TECHFORING/TECHFORING.jsx
@@ -4,7 +4,7 @@ export default function TECHFORING(){
     return(
         <>
         <div>
-            <h1 className="text-[#5BBC2E]">LIFE AT TECHFORING</h1>
+            <h1 className="text-[#5BBC2E]">LIFE AT TECHFORING Add</h1>
         </div>
         </>
     )
